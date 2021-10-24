b8d486cf34a48838ede0da01b3053947e0bddc54
AntoineKibok <73173590+AntoineKibok@users.noreply.github.com>
2021-10-24 12:04:53 +0200

    Début pause

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject togglePause;
+    public GameObject menu;
+
     void Start()
     {
         
@@ -20,8 +23,41 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
 
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menu.activeSelf == false)
+            {
+                Pause();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
     }
+
+    public void Restart()
+    {
+        UnPause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Pause()
+    {
+        togglePause.SetActive(false);
+        menu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnPause()
+    {
+        togglePause.SetActive(true);
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+
 }
